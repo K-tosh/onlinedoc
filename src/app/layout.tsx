5bc56ba08fca29e9a3329e71b7877bbc7da21261
K-tosh <ericmusanyi8gmail.com>
2026-03-05 23:31:59 +0300

    AddedAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "OnlineDoc — Online Health Services | Coming Soon",
